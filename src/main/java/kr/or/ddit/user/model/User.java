79a923ca5ca6b4d3ec1522b60cee65929f57c16b
jms <mingsoo0807@gmail.com>
2019-09-06 11:30:08 +0900

    [20190906] 1. HttpSessionBindingListener 테스트

--- src/main/java/kr/or/ddit/user/model/User.java
+++ src/main/java/kr/or/ddit/user/model/User.java
@@ -3,47 +3,72 @@ package kr.or.ddit.user.model;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import kr.or.ddit.encrypt.kisa.sha256.KISA_SHA256;
 
-public class User {
-	private String userId;	// 사용자 아이디
-	private String pass;	// 사용자 비밀번호
-	private String userNm;	// 사용자 이름
-	private String alias;	// 별명
-	private Date reg_dt;	// 등록일
-	private String addr1;	// 주소1
-	private String addr2;	// 주소2
-	private String zipcode;	// 우편번호
-	private String filename;	// 실제 파일명(사용자 업로드 파일명)
-	private String realfilename;// 물리 파일명(저장된 파일명)
-	private String realfilename2;// 물리 파일명(저장된 파일명)
-
+public class User implements HttpSessionBindingListener{
+	private static final Logger logger = LoggerFactory.getLogger(User.class);
+	
+	private String userId;		//사용자 아이디
+	private String pass;		//사용자 비밀번호
+	private String userNm;		//사용자 이름
+	private String alias;		//별명
+	private Date reg_dt;		//등록일
+	private String addr1;		//주소1
+	private String addr2;		//주소2
+	private String zipcode;		//우편번호
+	private String filename;		//파일명(사용자 업로드 파일명) 
+	private String realfilename;	//물리 파일명
+	private String realfilename2;	//물리 파일명
+	
 	public User() {
 		
 	}
-
-	public User(String userId, String pass, String userNm, Date reg_dt, String alias) {
-		this.userId = userId;
-		this.pass = pass;
-		this.userNm = userNm;
-		this.reg_dt = reg_dt;
-		this.alias = alias;
+	
+	public User(String userName) {
+		this.userNm = userName;
 	}
 	
-	public User(String userId, String pass, String userNm, String alias, Date reg_dt, String addr1, String addr2,
-			String zipcode, String filename, String realfilename) {
+	public User(String userId, String userNm, String alias, Date reg_dt, String addr1, String addr2,
+			String zipcode, String pass, String filename, String realfilename) {
 		this.userId = userId;
-		this.pass = pass;
 		this.userNm = userNm;
 		this.alias = alias;
 		this.reg_dt = reg_dt;
 		this.addr1 = addr1;
 		this.addr2 = addr2;
 		this.zipcode = zipcode;
+		this.pass = pass;
 		this.filename = filename;
 		this.realfilename = realfilename;
 	}
 
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public Date getReg_dt() {
+		return reg_dt;
+	}
+	
+	public String getReg_dt_fmt() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		return sdf.format(reg_dt);
+	}
+
+	public void setReg_dt(Date reg_dt) {
+		this.reg_dt = reg_dt;
+	}
+	
 	public String getUserNm() {
 		return userNm;
 	}
@@ -66,27 +91,6 @@ public class User {
 
 	public void setPass(String pass) {
 		this.pass = pass;
-	}
-
-	public Date getReg_dt() {
-		return reg_dt;
-	}
-	
-	public String getReg_dt_fmt() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		return sdf.format(reg_dt);
-	}
-
-	public void setReg_dt(Date reg_dt) {
-		this.reg_dt = reg_dt;
-	}
-
-	public String getAlias() {
-		return alias;
-	}
-
-	public void setAlias(String alias) {
-		this.alias = alias;
 	}
 
 	public String getAddr1() {
@@ -112,7 +116,7 @@ public class User {
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
 	}
-
+	
 	public String getFilename() {
 		return filename;
 	}
@@ -145,10 +149,22 @@ public class User {
 	}
 
 	public boolean checkLoginValidate(String userId, String pass) {
-		// 암호화 문장끼리 비교
-		if(userId.equals(this.userId) && KISA_SHA256.encrypt(pass).equals(this.pass)) {
+		
+		//암호화 문장끼리 비교
+		if(userId.equals(this.userId) && KISA_SHA256.encrypt(pass).equals(this.pass))
 			return true;
-		}
+		
 		return false;
 	}
+
+	@Override
+	public void valueBound(HttpSessionBindingEvent event) {
+		logger.debug("value bound");
+	}
+
+	@Override
+	public void valueUnbound(HttpSessionBindingEvent event) {
+		logger.debug("value unbound");
+	}
+	
 }
